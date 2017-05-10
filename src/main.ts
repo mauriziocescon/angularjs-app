e73d43b6ffc93723969a8d0570be027d89fadace
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-05-11 00:36:43 +0200

    add exception

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+/* tslint:disable:no-string-literal */
 import * as $ from "jquery";
 window["$"] = window["jQuery"] = $; // jQuery is global for other objs
+/* tslint:enable:no-string-literal */
+
 import * as angular from "angular";
 
 import "angular-animate";
