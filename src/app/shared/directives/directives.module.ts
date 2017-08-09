045e32f0893357d501ca66104a4cd8920852a200
Maurizio Cescon <Maurizio.CESCON@ext.ec.europa.eu>
2017-08-09 11:16:34 +0200

    localization improvements

--- src/app/shared/directives/directives.module.ts
+++ src/app/shared/directives/directives.module.ts
@@ -3,7 +3,6 @@ import * as angular from "angular";
 import { mcCustomValidatorDirective } from "./custom-validator.directive";
 import { mcFallbackImageUrlDirective } from "./fallback-image-name.directive";
 import { mcFocusIfDirective } from "./focus-if.directive";
-import { mcLocalizedStringDirective } from "./localized-string.directive";
 import { mcOnMouseWheelDirective } from "./on-mouse-wheel.directive";
 import { mcOnScrollDirective } from "./on-scroll.directive";
 import { mcScrollToTopDirective } from "./scroll-to-top.directive";
@@ -14,7 +13,6 @@ export const mcDirectives = angular.module("shared.mcDirectives", [])
     .directive("mcCustomValidator", mcCustomValidatorDirective)
     .directive("mcFallbackImageUrl", mcFallbackImageUrlDirective)
     .directive("mcFocusIf", mcFocusIfDirective)
-    .directive("mcLocalizedString", mcLocalizedStringDirective)
     .directive("mcOnMouseWheel", mcOnMouseWheelDirective)
     .directive("mcOnScroll", mcOnScrollDirective)
     .directive("mcScrollToTop", mcScrollToTopDirective)
